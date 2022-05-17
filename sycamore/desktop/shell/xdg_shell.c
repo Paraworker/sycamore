b6b18c7a7efe506db27fa284a5599b2dc9fbbbc0
Paraworker <paraworker@gmail.com>
2022-05-17 17:25:07 +0800

    xdg_shell: create xdg_shell with version 2

--- sycamore/desktop/shell/xdg_shell.c
+++ sycamore/desktop/shell/xdg_shell.c
@@ -277,7 +277,7 @@ struct sycamore_xdg_shell *sycamore_xdg_shell_create(struct sycamore_server *ser
 
     xdg_shell->server = server;
 
-    xdg_shell->wlr_xdg_shell = wlr_xdg_shell_create(display);
+    xdg_shell->wlr_xdg_shell = wlr_xdg_shell_create(display, 2);
     if (!xdg_shell->wlr_xdg_shell) {
         wlr_log(WLR_ERROR, "Unable to create_wlr_xdg_shell");
         free(xdg_shell);
