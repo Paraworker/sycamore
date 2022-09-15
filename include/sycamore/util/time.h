e082341b5ba955dcf8d700bff7c0903347eaa254
Paraworker <paraworker@gmail.com>
2022-09-15 23:03:06 +0800

    util: make get_current_time_msec() inline

--- include/sycamore/util/time.h
+++ include/sycamore/util/time.h
@@ -2,7 +2,12 @@
 #define SYCAMORE_TIME_H
 
 #include <stdint.h>
+#include <time.h>
 
-uint32_t get_current_time_msec();
+static inline uint32_t get_current_time_msec() {
+    struct timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    return now.tv_sec * 1000 + now.tv_nsec / 1000000;
+}
 
 #endif //SYCAMORE_TIME_H
--- sycamore/util/time.c
+++
@@ -1,8 +0,0 @@
-#include <time.h>
-#include "sycamore/util/time.h"
-
-uint32_t get_current_time_msec() {
-    struct timespec now;
-    clock_gettime(CLOCK_MONOTONIC, &now);
-    return now.tv_sec * 1000 + now.tv_nsec / 1000000;
-}
